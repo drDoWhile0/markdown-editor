450ea585008ef28153ab0e727e60072798b75468
David Candelaria <davidcandelaria@Davids-MacBook-Pro.local>
2026-04-13 23:50:28 -0600

    feature: import Preview prop into App.tsx - import marked markdown editor library

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import { useState } from 'react'
+import { marked } from 'marked';
 import MarkdownEditor from './components/MarkdownEditor'
+import Preview from './components/Preview';
 import './App.css'
 
 function App() {
   const [markdownContent, setMarkdownContent] = useState('# Hello there');
+  const markDownPreviewContent = marked.parse(markdownContent) as string;
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMarkdownContent(e.target.value);
     console.log(e.target.value);
-    
   }
 
   return (
-    <MarkdownEditor value={markdownContent} onChange={handleChange} />
+    <div className='markdown-editor-preview__container'>
+      <MarkdownEditor value={markdownContent} onChange={handleChange} />
+      <Preview _html={markDownPreviewContent } />
+    </div>
   )
 }
 
