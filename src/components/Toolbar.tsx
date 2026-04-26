0b3459df5f654ecdd282a358884ade1addc77258
David Candelaria <davidcandelaria@Davids-MacBook-Pro.local>
2026-04-26 17:26:13 -0600

    feature: wire up the hamburger menu button with the onToggleSidebar function call

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -7,7 +7,7 @@ import codeIcon from '../assets/icons/Code.png';
 import terminalIcon from '../assets/icons/Terminal.png';
 import previewIcon from '../assets/icons/Preview.png';
 
-function Toolbar({ onClick, onSave, onTogglePreview, saveStatus }: ToolbarProps) {
+function Toolbar({ onClick, onSave, onTogglePreview, onToggleSidebar, saveStatus }: ToolbarProps) {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
@@ -15,7 +15,7 @@ function Toolbar({ onClick, onSave, onTogglePreview, saveStatus }: ToolbarProps)
 
             <div className="self-end flex items-center">
 
-                <button onClick={() => setIsOpen(!isOpen)} className="cursor-pointer px-[6px] mx-[16px] flex flex-col gap-1">
+                <button onClick={() => { setIsOpen(!isOpen); onToggleSidebar(); }} className="cursor-pointer px-[6px] mx-[16px] flex flex-col gap-1">
                     <span className={`block w-6 h-0.5 bg-[#474747] transition-all duration-300 ${isOpen ? 'rotate-45 translate-y-1.5' : ''}`} />
                     <span className={`block w-6 h-0.5 bg-[#474747] transition-all duration-300 ${isOpen ? 'opacity-0' : ''}`} />
                     <span className={`block w-6 h-0.5 bg-[#474747] transition-all duration-300 ${isOpen ? '-rotate-45 -translate-y-1.5' : ''}`} />
